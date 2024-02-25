d0f891778548e1721efb81428991e3b0f1ebc02f
VignewaranEngineer <vigneswaranengineer3063@gmail.com>
2024-02-25 18:34:21 +0530

    polymorphism

---
+++ Product.java
@@ -0,0 +1,24 @@
+/* Develop an inventory management system for a store. 
+
+Create a class 'Product with attributes like name and price. 
+
+Implement a polymorphic collection to store different types of products.
+
+- Define a class 'Product' with attributes like name and price.
+
+- Implement subclasses for different types of products.
+
+- Use polymorphism to create a collection that can store various product types.
+
+*/
+
+public class Product {
+    String name;
+    double price;
+
+}
+
+
+class Soap extends Product{
+    
+}
