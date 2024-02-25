db19a931a9f2fbfbf34584ca6be4ebb371bb53bd
VignewaranEngineer <vigneswaranengineer3063@gmail.com>
2024-02-25 14:21:38 +0530

    small intro on arrays

---
+++ array.java
@@ -0,0 +1,15 @@
+
+// introduction to array
+
+public class array {
+
+    public static void main(String args[]){
+        //array arr1 = new array();
+        int[] arr = {1,2,3,4,5,6,7,8,9,9};
+
+        System.out.println("the elements are : ");
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
